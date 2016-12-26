d8b63c87d64d5fd1d6e57680969a3a65c9f86e87
Yo Vannaravuth <v.yo@gl-f.com>
2016-12-26 15:41:58 +0700

    temp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,48 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+        data: {
+            title: 'title of the home'
+        }
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: '**',
+        component: PageNotFoundComponent
+    }
+];
 
 @NgModule({
     declarations: [
         AppComponent,
-        HomeComponent
+        HomeComponent,
+        AboutComponent,
+        PageNotFoundComponent
     ],
     imports: [
         BrowserModule,
         FormsModule,
-        HttpModule
+        HttpModule,
+        RouterModule.forRoot(appRoutes)
     ],
     providers: [],
     bootstrap: [AppComponent]
